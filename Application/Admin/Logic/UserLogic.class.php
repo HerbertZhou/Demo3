26642b7ac15fbd56dc35a7f3ff9a2673aa56821b
mengtingting <857334730@qq.com>
2017-11-22 15:27:10 +0800

    哦哦哦哦

---
+++ Application/Admin/Logic/UserLogic.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tingting
+ * Date: 2017/10/25
+ * Time: 17:26
+ */
+
+namespace Think\Logic;
+
+
+use Think\Model;
+
+class UserLogic extends Model {
+
+
+
+}
